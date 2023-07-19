de09f9eb87d5530e11dddb3ba6d43660b97ed1b6
Kangping Xu <xukp20@mails.tsinghua.edu.cn>
2023-07-19 11:29:41 +0800

    update

--- crawler/libgen/batch_wget.py
+++ crawler/libgen/batch_wget.py
@@ -7,29 +7,39 @@ import json
 import argparse
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from tqdm import tqdm
-MAX_THREADS = 4
+MAX_THREADS = 2
 
 def get_parser():
     parser = argparse.ArgumentParser(description='Download batch of books from an index.json in a dir by wget')
-    parser.add_argument('-i', '--input', type=str, help='input dir', default='/data/xukp/libgen/gre math')
+    parser.add_argument('-i', '--input', type=str, help='input dir', default='/data/xukp/libgen/gre_math')
     parser.add_argument('-f', '--index_file', type=str, help='index file', default='index.json')
+    parser.add_argument('-q', '--quiet', action='store_true', help='quiet')
     
     return parser
 
-def download_book(book):
+import time
+import random
+def download_book(book, quiet=False):
+    # random sleep to avoid being banned
+    delay = random.randint(1, 5)
+    time.sleep(delay)
+
     urls = book['urls']
     for url in urls:
-        cmd = f'wget -c {url}'
-        # if success, break
-        if os.system(cmd) == 0:
-            return True, book['id']
+        tries = 0
+        while tries < 3:
+            cmd = f'wget -c {url} > /dev/null 2>&1' if quiet else f'wget -c {url}'
+            # if success, break
+            if os.system(cmd) == 0:
+                return True, book['id']
+            tries += 1
     tqdm.write(f'Failed to download book {book["book_title"]}')
     return False, book['id']
 
 
-def download_book_multi_threaded(index):
+def download_book_multi_threaded(index, quiet):
     with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
-        futures = [executor.submit(download_book, book) for book in index if book['downloaded'] == False]
+        futures = [executor.submit(download_book, book, quiet) for book in index if book['download'] == False]
 
         count = 0
         # Process the results as they complete
@@ -39,7 +49,7 @@ def download_book_multi_threaded(index):
                 count += 1
                 for book in index:
                     if book['id'] == id:
-                        book['downloaded'] = True
+                        book['download'] = True
                         break
         
         print(f'Downloaded {count} books out of {len(futures)} tries.')
@@ -51,13 +61,14 @@ if __name__ == '__main__':
     args = get_parser().parse_args()
     input_dir = args.input
     index_file = args.index_file
+    quiet = args.quiet
 
     with open(os.path.join(input_dir, index_file), 'r') as f:
         index = json.load(f)
     
     # move to input dir
     os.chdir(input_dir)
-    download_book_multi_threaded(index)
+    download_book_multi_threaded(index, quiet)
     
             
 
