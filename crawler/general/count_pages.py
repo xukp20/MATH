61365701651c33914760e353268acc492be997bf
Kangping Xu <xukp20@mails.tsinghua.edu.cn>
2023-07-19 22:19:16 +0800

    add tool

---
+++ crawler/general/count_pages.py
@@ -0,0 +1,34 @@
+import os
+import PyPDF2
+
+def count_pages_in_pdf(pdf_file):
+    with open(pdf_file, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        return len(reader.pages)
+
+def count_pages_in_directory(directory_path):
+    total_pages = 0
+    for root, dirs, files in os.walk(directory_path):
+        for filename in files:
+            if filename.lower().endswith('.pdf'):
+                pdf_file_path = os.path.join(root, filename)
+                total_pages += count_pages_in_pdf(pdf_file_path)
+    return total_pages
+
+
+import argparse
+def get_parser():
+    parser = argparse.ArgumentParser(description='Count pages in PDF files in a dir')
+    parser.add_argument('-p', '--path', type=str, help='path', default=None)
+
+    return parser
+
+if __name__ == '__main__':
+    args = get_parser().parse_args()
+    directory_path = args.path
+    if directory_path is None:
+        print('No path specified.')
+        exit(0)
+        
+    total_pages = count_pages_in_directory(directory_path)
+    print("Total pages of all PDF files in the directory (including subdirectories):", total_pages)
