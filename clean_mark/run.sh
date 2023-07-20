86591e756b21492d60bae038e513684219448653
Kangping Xu <xukp20@mails.tsinghua.edu.cn>
2023-07-20 15:32:37 +0800

    bash

---
+++ clean_mark/run.sh
@@ -0,0 +1,21 @@
+!#/bin/bash
+
+BUILD=1
+BROKEN=1
+DUP=1
+RELATED=1
+
+if [ $BUILD -eq 1 ]; then
+    echo "Building"
+    python build_index.py -f include_dirs.json
+fi
+
+if [ $BROKEN -eq 1 ]; then
+    echo "Finding broken"
+    python broken.py
+fi
+
+if [ $DUP -eq 1 ]; then
+    echo "Finding duplicates"
+    python duplicate.py
+fi
